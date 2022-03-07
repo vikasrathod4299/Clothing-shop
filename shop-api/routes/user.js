0c0f1a352dd63c6ec7b9b562a44211bf67410f16
Vikas Rathod <vikas.rv4299@gmail.com>
2022-03-07 21:43:11 +0530

    Added user routs

--- shop-api/routes/user.js
+++ shop-api/routes/user.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const bcrypt = require('bcrypt')
+const { json } = require('body-parser')
 const User = require('../models/User')
 const {verfyToken,verifyTokenAuthentication, verifyTokenAdminAuth} = require("./verifyToken")
 
@@ -57,15 +58,43 @@ router.get("/find/:id",verifyTokenAdminAuth, async(req,res)=>{
 
 //GET ALL USER (ADMIN)
 router.get("/",verifyTokenAdminAuth, async(req,res)=>{
-    try{
-        
-        user = await User.find();
+    const query = req.query.new 
+    try{   
+        const user = query ? await User.find().sort({_id:-1}).limit(3) : await User.find();
         // const {password,...others} = user._doc;
         res.status(200).json(user)
     }catch(err)
     {
         res.status(500).json(err)
     }
+})
+
+//GET USER STATS (ADMIN)
+router.get("/stats",verifyTokenAdminAuth, async(req,res)=>{
+    const date =  new Date();
+    const lastYear = new Date(date.setFullYear(date.getFullYear() -1));
+
+    try{
+        const data = await User.aggregate([
+            { $match: { createdAt: {$gte: lastYear } } },
+            {
+                $project:{
+                    month:{$month: "$createdAt"},
+                }
+            },
+            {
+                $group:{
+                    _id:"$month",
+                    total:{$sum:1}
+                }
+            }
+
+        ]) 
+        res.status(200).json(data);       
+    }catch(err){
+        res.status(500).json(err)
+    }
+
 })
 
 module.exports = router;
