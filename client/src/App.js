e34feb0f9ae7e83825d0c79e13cb18a8fa09f008
Vikas Rathod <vikas.rv4299@gmail.com>
2022-03-17 23:51:00 +0530

    built cart page and product page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Cart from './pages/Cart'
+// import Login from './pages/Login.jsx'
 // import Home from './pages/Home'
 // import ProductList from './pages/ProductList'
-import Product from './pages/Product'
+// import Product from './pages/Product'
+// import Register from './pages/Register'
 
 const App = () => {
-  return <Product/>
+  return <Cart/>
 }
 
 export default App
